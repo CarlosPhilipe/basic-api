05b8d74e415b908e123e6b0517e6a3b2f29653d8
Carlos Philipe <carlos.bahia@meliuz.com.br>
2018-11-30 10:18:21 -0400

    Adiciona nodemon ao projeto

--- server.js
+++ server.js
@@ -10,14 +10,20 @@ app.set('port', port);
 
 const server = http.createServer(app);
 const router = express.Router();
-
 var route = router.get('/', (req, res, next) => {
   res.status(200).send({
     title: "Node API",
     version: "0.0.1"
   });
 });
+var pin = router.get('/pins/', (req, res, next) => {
+  res.status(200).send({
+    title: "Pins",
+    version: "0.0.1"
+  });
+});
 
 app.use('/', route);
+app.use('/', pin);
 server.listen(port);
 console.log('API rodando na porta ' + port);
