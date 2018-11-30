a2b7a7d91091f54254679eddc2b1319660dce8cc
Carlos Philipe <carlos.bahia@meliuz.com.br>
2018-11-30 10:10:03 -0400

    Adiciona server no projeto

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-// console.log('testando')
-//
-// const http = require('http');
-// const express = require('express');
-// const debug = require('debug')('nodestr:server');
-//
-// const app = express();
-// const port = 3000;
-// app.set('port', port);
-//
-// const server = http.createServer(app);
-// const router = express.Router();
-//
-// var route = router.get('/', (req, res, next) => {
-//   res.status(200).send({
-//     title: "Node API",
-//     version: "0.0.1"
-//   });
-// });
-//
-// app.use('/', route);
-// server.listen(port);
-// console.log('API rodando na porta ' + port);
+console.log('testando')
+
+const http = require('http');
+const express = require('express');
+const debug = require('debug')('nodestr:server');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router();
+
+var route = router.get('/', (req, res, next) => {
+  res.status(200).send({
+    title: "Node API",
+    version: "0.0.1"
+  });
+});
+
+app.use('/', route);
+server.listen(port);
+console.log('API rodando na porta ' + port);
